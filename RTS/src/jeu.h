37cf97ac04a54b55ed58b5ff106c61a5c707e371
Andzura <adrien.baud@gmail.com>
2015-03-29 20:13:36 +0200

    diagmodules2

---
+++ RTS/src/jeu.h
@@ -0,0 +1,21 @@
+/**
+ * \file uniteBase.h
+ * \brief module jeu
+ * \author Adrien Baud & Diego Roussel
+ * \date 24/03/15
+ *
+ * Module du coeur du jeu (boucle de jeu/ affichage/gestion evenement)
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "uniteBase.h"
+#include "unite.h"
+#include "case.h"
+
+#ifndef
+#define _JEU
+
+
+#endif
